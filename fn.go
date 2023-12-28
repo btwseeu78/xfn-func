c84f4c28df7830bebe3d9da33624596da547a79d
Arpan Chatterjee <btwseeu78@gmail.com>
2023-12-28 13:37:04 +0530

    Added All

--- fn.go
+++ fn.go
@@ -69,10 +69,10 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 			err = patchFieldValueToObject(obj.FieldPath, obj.Prefix+randString, desired[resource.Name(obj.Name)].Resource)
 		} else {
 			f.log.Info("here on before error", "resname", desired[resource.Name(obj.Name)].Resource, "path", obj.FieldPath)
-			err = patchFieldValueToObject(obj.FieldPath, randString, desired[resource.Name(obj.Name)].Resource)
-			if err != nil {
-				f.log.Debug("Error", "is", err, "obj", obj.FieldPath)
-			}
+			//err = patchFieldValueToObject(obj.FieldPath, randString, desired[resource.Name(obj.Name)].Resource)
+			//if err != nil {
+			//	f.log.Debug("Error", "is", err, "obj", obj.FieldPath)
+			//}
 		}
 	}
 	err = response.SetDesiredComposedResources(rsp, desired)
